21eda060127298bf0e3cc5c35ec3d46449678f9a
positiveWand <49838556+positiveWand@users.noreply.github.com>
2019-12-10 01:10:39 +0900

    join버튼 서버동작 구현

--- hw2_script.js
+++ hw2_script.js
@@ -6,38 +6,72 @@ $(document).ready(function() {
   $("#currentID").text("");
 
   $("#joinButton").click(function() {
-    $(".loginBox").show();
+    $(".joinBox").show();
   });
 
-  $("#loginSubmit").click(function() {
+  $("#joinSubmit").click(function() { //"로그인" 시도
     var id = $("#inputID").val();
     var psw = $("#inputPSW").val();
     if(id == "" || psw == "" || !validateID(id) || !validatePSW(psw)) {
       //정상적인 입력이 아닌 경우
+
+      //경고 메세지
       alert("아이디 또는 패스워드의 입력양식을 체크해주세요.");
+      //input공간 초기화
       $("#inputID").val("");
       $("#inputPSW").val("");
-      $(".loginBox").hide();
-      
+      //loginBox 감추기
+      $(".joinBox").hide();
     }
     else {
       //정상적인 입력인 경우 -> 정상처리
+      $.ajax({
+        url: "./joinRequest.php",
+        type: "post",
+        data: $("joinForm").serialize(),
+      }).done(function(data) {
+        if(data == "success") {
+          alert("로그인 성공");
+          //로그인 사용자ID 표시
+          //사용자 데이터 출력
+        }
+        else {
+          alert("로그인 실패");
+        }
+      });
     }
   });
 
-  $("#loginSignin").click(function() {
+  $("#joinSignin").click(function() { //"회원가입" 시도
     var id = $("#inputID").val();
     var psw = $("#inputPSW").val();
     if(id == "" || psw == "" || !validateID(id) || !validatePSW(psw)) {
       //정상적인 입력이 아닌 경우
+
+      //경고 메세지
       alert("아이디 또는 패스워드의 입력양식을 체크해주세요.");
+      //input공간 초기화
       $("#inputID").val("");
       $("#inputPSW").val("");
-      $(".loginBox").hide();
-      
+      //loginBox 감추기
+      $(".joinBox").hide();  
     }
     else {
       //정상적인 입력인 경우 -> 정상처리
+      $.ajax({
+        url: "./joinRequest.php",
+        type: "post",
+        data: $("joinForm").serialize(),
+      }).done(function(data) {
+        if(data == "success") {
+          alert("회원가입 성공");
+          alert("회원가입이 완료되었습니다.")
+        }
+        else {
+          alert("회원가입 실패");
+          alert("아이디가 중복됩니다. 다시 회원가입해주세요.");
+        }
+      });
     }
   });
 
---
+++ joinRequest.php
@@ -0,0 +1,68 @@
+<?php
+$request = $_POST["request"]; //요청 종류
+$userID = $_POST["userID"]; //유저 아이디
+$userPSW = $_POST["userPSW"]; //유저 비밀번호
+$userFound = false;
+
+$result = "fail";
+
+
+
+//유저 정보 읽어오기
+$usersArray = array(); //전체 유저 정보가 저장되는 array
+$userFile = fopen("./data/person.txt", "r");
+while(!feof($userFile)) {
+    $anUser = fgets($userFile); //한 줄(=1명의 유저) 읽기
+    if(empty($anUser)) {
+        break;
+    }
+    $userInfo = explode("||", $anUser); //구분자("||")를 이용해 정보 분리
+    //유저 정보 (index : 0 -> 아이디, index : 1 -> 비밀번호)
+    array_push($usersArray, $userInfo);
+}
+fclose($userFile);
+
+$numOfUsers = count($usersArray, 0); //총 유저의 수
+
+
+
+$userFound = findUser($usersArray, $userID, $userPSW);
+
+if($request == "Submit") {
+    if($userFound) {
+        $result = "success";
+    }
+    else {
+        $result = "fail";
+    }
+} else if($request == "Signin") {
+    if($userFound) { //이미 등록된 정보 있음(회원가입 실패)
+        $result = "fail";
+    }
+    else { //등록된 정보가 없을 경우(회원가입 성공)
+        //회원가입 진행(유저 정보 기록)
+        $userFile = fopen("./data/person.txt", "a+");
+        fwrite($userFile, $userID."||");
+        fwrite($userFile, $userPSW."\n");
+        fclose($userFile);
+        $result = "success";
+    }
+}
+
+echo $result; //클라이언트에 정보 전송
+
+function findUser($anArray, $anID, $anPSW) {
+    $result = false;
+
+    foreach($anArray as $anUser) {
+        $id = $anUser[0];
+        $psw = $anUser[1];
+
+        if($id == $anID, $psw == $anPSW) {
+            $result = true;
+        }
+    }
+
+    return $result;
+}
+?>
