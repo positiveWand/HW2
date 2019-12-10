2386e29f5fea4ae542dd4627f54b5190dc028977
positiveWand <49838556+positiveWand@users.noreply.github.com>
2019-12-10 16:54:52 +0900

    ㅇㄹㅇㄹㅇ

--- hw2_script.js
+++ hw2_script.js
@@ -1,5 +1,8 @@
 var logedIn = false;
 
+var addShown = false;
+var joinShown = false;
+
 var aCalandar = {
   "Sun" : [],
   "Mon" : [],
@@ -10,6 +13,8 @@ var aCalandar = {
   "Sat" : []
 };
 
+var currentWork = null;
+
 $(document).ready(function() {
 
   //초기 설정
@@ -19,7 +24,14 @@ $(document).ready(function() {
 
   $("#addButton").click(function() {
     if(logedIn) {
-      $(".addBox").show();
+      if(!addShown) {
+        $(".addBox").show();
+        addShown = true;
+      }
+      else {
+        $(".addBox").hide();
+        addShown = false;
+      }
     }
     else {
       alert("추가하기 위해 로그인을 해주세요.")
@@ -27,23 +39,58 @@ $(document).ready(function() {
   });
 
   $("#joinButton").click(function() {
-    $(".joinBox").show();
+    if(!joinShown){
+      $(".joinBox").show();
+      joinShown = true;
+    }
+    else {
+      $(".joinBox").hide();
+      joinShown = false;
+    }
+  });
+
+  $(".works").click(function() {
+    $("#editDay").attr("disabled", true);
+    $("#editTitle").attr("disabled", true);
+    $("#editDescription").attr("disabled", true);
+
+    $("$editEdit").attr("disabled",false);
+    $("$editDelete").attr("disabled",false);
+    $("#editSubmit").attr("disabled", true);
+    $("$editCancel").attr("disabled",false);
+
+    var thisID = $(this).attr("id");
+    var thisDay = $(this).parent().attr("id").replace("List", "");
+    currentWork = findWork(thisID);
+    var thisTitle = currentWork["title"];
+    var thisDescription = currentWork["description"];
+
+    $("#editDay").val(thisDay);
+    $("#editTitle").val(thisTitle);
+    $("#editDescription").val(thisDescription);
+
+
+    $(".editBox").show();
   });
 
   //$(".updateBox").show();
 
   //"AddBox"관련 처리
+  //"Save" 버튼
   $("#addSave").click(function() {
     var day = $("#inputDay").val();
     var title = $("#inputTitle").val();
     var description = $("#inputDescription").val();
     var id = $("#currentID").text();
 
+    var currentDate = new Date();
+
     if(title == "" || description == "") { //정상적인 입력이 아닌 경우
       alert("할 일의 제목과 내용을 입력해주세요.");
     }
     else { //정상적인 입력인 경우
       //입력된 내용 서버에 저장
+      /*
       alert($("#addForm").serialize());
       $.ajax({
         url: "./addRequest.php",
@@ -57,16 +104,22 @@ $(document).ready(function() {
           showCalandar();
         }
       });
+      */
+      appendWork(day, currentDate.getDate(), title, description);
+      saveCalandar();
+      alert("저장되었습니다.");
     }
 
   });
-
+  //"Cancel"버튼
   $("#addCancel").click(function() {
     $(".addBox").hide();
+    addShown = false;
   });
 
 
   //"JoinBox"관련 처리
+  //"Submit"버튼
   $("#joinSubmit").click(function() { //"로그인" 시도
     var id = $("#inputID").val();
     var psw = $("#inputPSW").val();
@@ -80,6 +133,7 @@ $(document).ready(function() {
       $("#inputPSW").val("");
       //loginBox 감추기
       $(".joinBox").hide();
+      joinShown = false;
     }
     else {
       //정상적인 입력인 경우 -> 정상처리
@@ -92,12 +146,14 @@ $(document).ready(function() {
         if(data == "success") {
           alert("로그인 성공");
           $(".joinBox").hide();
+          joinShown = false;
 
           logedIn = true;
 
           //로그인 사용자ID 표시
           $("#currentID").text(id);
           //사용자 데이터 출력
+          getCalandar();
           showCalandar();
         }
         else {
@@ -107,7 +163,7 @@ $(document).ready(function() {
       });
     }
   });
-
+  //"Signin"버튼
   $("#joinSignin").click(function() { //"회원가입" 시도
     var id = $("#inputID").val();
     var psw = $("#inputPSW").val();
@@ -120,7 +176,8 @@ $(document).ready(function() {
       $("#inputID").val("");
       $("#inputPSW").val("");
       //loginBox 감추기
-      $(".joinBox").hide();  
+      $(".joinBox").hide(); 
+      joinShown = false;
     }
     else {
       //정상적인 입력인 경우 -> 정상처리
@@ -142,6 +199,42 @@ $(document).ready(function() {
     }
   });
 
+  //"EditBox"관련 처리
+  //"Edit"버튼
+  $("#editEdit").click(function() {
+    $("#editDay").attr("disabled", false);
+    $("#editTitle").attr("disabled", false);
+    $("#editDescription").attr("disabled", false);
+
+    $("$editEdit").attr("disabled",true);
+    $("$editDelete").attr("disabled",true);
+    $("#editSubmit").attr("disabled", false);
+    $("$editCancel").attr("disabled",false);
+  });
+  //"Delete"버튼
+  $("#editDelete").click(function() {
+    var currentID = currentWork["id"];
+    popWork(currentID);
+    saveCalandar();
+    alert("삭제되었습니다.");
+    $(".editBox").hide();
+    showCalandar();
+  });
+  //"Submit"버튼
+  $("#editSubmit").click(function() {
+    currentWork["title"] = $("#editTitle").val();
+    currentWork["description"] = $("#edit").val();
+    aCalandar[findDay(currentWork["id"])].push(popWork(currentWork["id"]));
+    saveCalandar();
+    alert("저장되었습니다.");
+    $(".editBox").hide();
+    showCalandar();
+  });
+  //"Cancel"버튼
+  $("#editCancel").click(function() {
+    $(".editBox").hide();
+  });
+
   
 });
 
@@ -155,7 +248,7 @@ function validatePSW(aString) {
   return pswPattern.test(aString);
 }
 
-function showCalandar() {
+function getCalandar() {
   $.ajax({
     url: "./readCalandar.php",
     type: "post",
@@ -167,21 +260,28 @@ function showCalandar() {
     {"Mon" : [works], "Tue" : [works], ... , Sun : [works]}
 
     Work.json
-    {"title" : "할 일 제목", "description" : "할 일 내용"}
+    {"id" : unique key(timestamp), "title" : "할 일 제목", "description" : "할 일 내용"}
     */
    //json파일을 해석하여 1. 달력에 표시, 2. 별도의 저장소에 저장
     var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    var haveData = false;
+
     for(var i = 0; i < 7; i++){
       var dayWorks = jsonData[days[i]];
       for(var j = 0; j < dayWorks.length; j) {
-        appendWork(days[i], dayWorks[j]["title"], dayWorks[j]["description"]);
+        haveData = true;
+        aCalandar[day].push({"id" : id, "title" : title, "description" : description});
       }
+    }
+
+    if(!haveData) {
+      alert("저장한 데이터가 없습니다.");
     }
   });
 }
-function appendWork(day, title, description) {
-  $("#"+day+"List").append("<li class=\"works\">"+title+"</li>");
-  aCalandar[day].push({"title" : title, "description" : description});
+
+function showCalandar() {
+  $("#"+day+"List").append("<li id=\""+id+"\" class=\"works\">"+title+"</li>");
 }
 
 function saveCalandar() {
@@ -196,4 +296,56 @@ function saveCalandar() {
 
 function clearCalandar() {
 
+}
+
+function findWork(workID) {
+  var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  var targetWork = null;
+
+  for(var i = 0; i < 7; i++){
+    var dayWorks = aCalandar[days[i]];
+    for(var j = 0; j < dayWorks.length; j) {
+      if(dayWorks[j]["id"] == workID) {
+        targetWork = dayWorks[j];
+        return targetWork;
+      }
+    }
+  }
+
+  return targetWork;
+}
+
+function popWork(workID) {
+  var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  var targetWork = null;
+
+  for(var i = 0; i < 7; i++){
+    var dayWorks = aCalandar[days[i]];
+    for(var j = 0; j < dayWorks.length; j) {
+      if(dayWorks[j]["id"] == workID) {
+        targetWork = dayWorks[j];
+        aCalandar.splice(j, 1);
+        return targetWork;
+      }
+    }
+  }
+
+  return targetWork;
+}
+
+function findDay(workID) {
+  var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  var targetDay = null;
+
+  for(var i = 0; i < 7; i++){
+    var dayWorks = aCalandar[days[i]];
+    for(var j = 0; j < dayWorks.length; j) {
+      if(dayWorks[j]["id"] == workID) {
+        targetDay = days[i];
+        return targetDay;
+      }
+    }
+  }
+
+  return targetDay;
 }
